ac8952019c9608ddfee6ddd21f5c3010cc2d785b
kimbodle <103980156+kimbodle@users.noreply.github.com>
2025-07-19 10:05:14 +0900

    add: TutorialController

---
+++ Assets/Script/UI/TutorialController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+public class TutorialController : MonoBehaviour
+{
+    [SerializeField] Image TutorialImage;
+    [SerializeField] Button leftButton;
+    [SerializeField] Button rightButton;
+    [SerializeField] Button closeButton;
+    private int index = 0;
+
+    public Sprite[] TutorialImages;
+
+    void Start()
+    {
+        leftButton.onClick.AddListener(OnClickLeftButton);
+        rightButton.onClick.AddListener(OnClickRightButton);
+        closeButton.onClick.AddListener(OnClickCloseButton);
+
+        TutorialImage.sprite = TutorialImages[index];
+    }
+    
+    void OnClickLeftButton()
+    {
+        if (index == 0) { return; }
+        TutorialImage.sprite = TutorialImages[index - 1];
+        index--;
+
+    }
+
+    void OnClickRightButton()
+    {
+        if (index == 2) { return; }
+        TutorialImage.sprite = TutorialImages[index + 1];
+        index++;
+    }
+    void OnClickCloseButton()
+    {
+        //Todo: 게임 매니저 메소드 호출(인게임 UI띄우고 게임 시작하기)
+        gameObject.SetActive(false);
+    }
+}
